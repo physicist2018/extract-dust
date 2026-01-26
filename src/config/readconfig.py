215df0e4c0e067ef790777d5d5592ebfbce06354
physicist2018 <physicist2018@yandex.ru>
2026-01-26 17:36:36 +1000

    Add read_config_from_json

--- src/config/readconfig.py
+++ src/config/readconfig.py
@@ -1,3 +1,4 @@
+import json
 from collections import namedtuple
 
 Config = namedtuple(
@@ -7,4 +8,12 @@ Config = namedtuple(
 
 
 def get_default_config() -> Config:
+    """Return the default configuration."""
     return Config(delta_d=0.31, delta_nd=0.05)
+
+
+def read_config_from_json(filepath: str) -> Config:
+    """Read configuration from a JSON file."""
+    with open(filepath, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    return Config(delta_d=data["delta_d"], delta_nd=data["delta_nd"])
